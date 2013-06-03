378d21c1360e98098ddf4d9e88ef58e01bb76900
root <root@nimatel.com>
2013-06-03 18:41:41 -0400

    Basic script.

--- scripts/nimatel.lua
+++ scripts/nimatel.lua
@@ -2,10 +2,19 @@ local dbh = freeswitch.Dbh("pgsql://hostaddr=127.0.0.1 dbname=nimatel_production
 assert(dbh:connected());
 
 session:answer();
-session:sleep(400);
 
-session:streamFile("nimatel/welcome.wav");
-session:streamFile("nimatel/enter_pin_or_cc.wav");
+digits = session:playAndGetDigits(12, 16, 1, 2000, "", "phrase:welcome_enter_pin_cc", "", "\\d+");
+
+if string.len(digits) == 12 then
+  dbh:query("SELECT * FROM cards WHERE pin='" .. digits .. "';", function(row)
+    freeswitch.consoleLog("info", "\n\nbalance = " .. row.balance .. "\n\n");
+  end);
+  freeswitch.consoleLog("info", "\n12 digits " .. digits .. "\n");
+elseif string.len(digits) == 16 then
+  freeswitch.consoleLog("info", "16 digits " .. digits .. "\n");
+else
+  freeswitch.consoleLog("info", "Exit " .. digits .. "\n");
+end
 
 session:hangup();
 
